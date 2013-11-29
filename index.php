48380e821ef111c5ad3df30489eddb33cb9fcf1e
Yves Courtois <Courtois1337@gmail.com>
2013-11-28 19:54:55 -0600

    added favicon

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <head>
 <title>The Tea Club</title> 
 <link rel="stylesheet" type="text/css" href="style.css">
-
+<link href="favicon.ico" rel="shortucut icon" />
 <?php 
     $i = 0; 
     $dir = 'img/';
@@ -13,9 +13,6 @@
         }
     }
 ?>
-<script type="text/javascript">
-	var count=<?php echo json_encode($i); ?>;
-</script>
 
 </head> 
 
@@ -23,31 +20,24 @@
 
 <div id="content" class="LR-content">
 	<div class="LR-bg-img_wrapper">
-		<div class="LR-bg-img-overlay " style="background-color: rgb(255, 255, 255); background-position: initial initial; background-repeat: initial initial;">
-		</div>
-		<div class="LR-bg-img LR-site-bg-image-container LR-site-show-on-bg-image">
+		<div class="LR-bg-img-overlay" style="background-color: rgb(255, 255, 255); background-position: initial initial; background-repeat: initial initial;"></div>
+		<div class="LR-bg-img LR-site-bg-image-container LR-site-show-on-bg-image"></div>
 	</div>
-	</div>
-	<div class="LR-box-wrapper">	
-		<div class="LR-box">
-			<div class="LR-box-container">
-				<h1 class="LR-site-title"> </h1>
-				<div class="LR-site-tagline  LR-clearfix"><p style="background-color: rgb(0, 0, 0); background-position: initial initial; background-repeat: initial initial;">The Tea Club</p>
-				</div>		
-				<div class="LR-box-inner">
-					<div class="LR-sign-up">
-						<div class="LR-site-description ">
-							<p>This club started with a small group of UT students drinking tea in their balcony. Wanting to bring new ideas and hoping to broaden discussions, the idea of inviting the student body to participate in casual philosophical conversations about anything was materialized.</p>
-							<p>The Tea Club provides a large variety of tea for you to drink, hot water and cup included. Just bring your favorite topic of discussion, and a friend.</p>
-							<p style="font-size: 90%;">Join the <a href="http://on.fb.me/1cpjJ0B">Facebook group</a> to learn more.</p>
-						</div>
-					</div>
-				</div>
+	<div class="LR-box-wrapper">
+		<div class="LR-box-container">
+			<div class="LR-site-tagline  LR-clearfix">
+				<p style="background-color: rgb(0, 0, 0); background-position: initial initial; background-repeat: initial initial;">The Tea Club</p>
+			</div>		
+			<div class="LR-site-description ">
+				<p style="font-size: 90%;">This club started with a small group of UT students drinking tea in their balcony. Wanting to bring new ideas and hoping to broaden discussions, the idea of inviting the student body to participate in casual philosophical conversations about anything was materialized.</p>
+				<p style="font-size: 90%;">The Tea Club provides a large variety of tea for you to drink, hot water and cup included. Just bring your favorite topic of discussion, and a friend.</p>
+				<p style="font-size: 70%;">Join the <a href="http://on.fb.me/1cpjJ0B">Facebook group</a> to learn more.</p>
 			</div>
 		</div>
 	</div>
 </div>  
 
+<script type="text/javascript">var count=<?php echo json_encode($i); ?>;</script>
 <script style="text/javascript" src="includes/jquery.min.js"></script>
 <script style="text/javascript" src="includes/background.js"></script>
 
